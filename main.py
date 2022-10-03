723d432af336326f6ecf9a794b46b1a093dc677c
VichiesUA <stasyatsyn@gmail.com>
2022-10-03 12:33:49 +0300

    it`s commit :)

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from faker import Faker
 
 fake = Faker()
 
-count_of_users = int(input('Введіть кількість користувачів ==>  '))
 
-while True:
-    dict_users_data = {f'{fake.user_name() + str(randint(0, 100000))}': (
-        (fake.password(
-            length=randint(5, 25), special_chars=False, digits=True, upper_case=True, lower_case=True))) for _ in range(
-        count_of_users)}
-    if len(dict_users_data) == count_of_users:
-        break
+def generate_users_data(count_of_users):
+    while True:
+        dict_users_data = {f'{fake.user_name() + str(randint(0, 100000))}': (
+            (fake.password(
+                length=randint(5, 25), special_chars=False, digits=True, upper_case=True, lower_case=True))) for _ in range(count_of_users)}
+        if len(dict_users_data) == count_of_users:
+            return dict_users_data
 
 
-print(dict_users_data)
-print(len(dict_users_data))
+users_count = int(input('Введіть кількість користувачів ==>  '))
+
+print(generate_users_data(users_count))
