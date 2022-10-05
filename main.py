3d0ff7e4817a619656e8df60b77c43a1a8dc506e
VichiesUA <stasyatsyn@gmail.com>
2022-10-05 18:16:36 +0300

    commot with function

--- main.py
+++ main.py
@@ -3,16 +3,43 @@ from faker import Faker
 
 fake = Faker()
 
+dict_users_data = {}
 
+
+# Тут генеруються логіни
+def generate_user_name():
+    yield f'{fake.user_name() + str(randint(0, 100000))}'
+
+
+# Тут генеруються паролі
+def generate_password():
+    yield fake.password(length=randint(5, 25), special_chars=False)
+
+
+# Тут генерується словник з логінами і паролями
 def generate_users_data(count_of_users):
-    while True:
-        dict_users_data = {f'{fake.user_name() + str(randint(0, 100000))}': (
-            (fake.password(
-                length=randint(5, 25), special_chars=False, digits=True, upper_case=True, lower_case=True))) for _ in range(count_of_users)}
-        if len(dict_users_data) == count_of_users:
-            return dict_users_data
+    for _ in range(count_of_users):
+        user_name = list(generate_user_name())
+        user_password = list(generate_password())
+        dict_users_data[user_name[0]] = user_password[0]
 
 
-users_count = int(input('Введіть кількість користувачів ==> '))
+# перевірка чи правильна кількість даних згенерувалась
+# якщо неправильна, то догенеровуються дані
+def validation(count_of_users):
+    if len(dict_users_data) == count_of_users:
+        # \n count ==> {len(dict_users_data)} - чисто для перевірки точності роботи скрипта
+        return f'{dict_users_data}, \n count ==> {len(dict_users_data)}'
+    users_count = count_of_users - len(dict_users_data)
+    generate_users_data(users_count)
+    return validation(count_of_users)
 
-print(generate_users_data(users_count))
+
+# Вивід результату в консоль
+def consol_result():
+    i = int(input('Введіть кількість користувачів ==> '))
+    print(validation(i))
+
+
+if __name__ == '__main__':
+    consol_result()
